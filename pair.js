a4c4475ed5c33d3e6a028d569a3ce5ccde8558ba
kaviyasd <Kavindhagihan79@gmail.com>
2025-07-09 14:59:23 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,7 +81,7 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+            const sid = `*Kaviya [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*https://wa.me/message/PHFSLOO4KCVCB1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/BxgyUSr5emZCJ6Hy5cM45t?mode=r_t*`;
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
