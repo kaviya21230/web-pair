8bc51eb03e6d3b86da51a656f79cd12a392bba62
kaviyasd <Kavindhagihan79@gmail.com>
2025-07-09 17:21:19 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://raw.githubusercontent.com/kaviya21230/Bot-Helper/main/autoimage/KING%20KAVIYA%20MD-V1.jpg",
               },
               caption: sid,
             });
